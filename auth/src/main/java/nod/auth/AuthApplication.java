69f2e3ed5e6c6ddb8ae37207588bf3ffeb05ecb2
minsu7426 <minsu7426@naver.com>
2025-07-29 00:14:39 +0900

    auth module > common 추가

--- auth/src/main/java/nod/auth/AuthApplication.java
+++ auth/src/main/java/nod/auth/AuthApplication.java
@@ -2,7 +2,12 @@ package nod.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {
+        "nod.auth",
+        "nod.common"
+})
 @SpringBootApplication
 public class AuthApplication {
 
